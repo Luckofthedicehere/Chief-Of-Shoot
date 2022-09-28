9b33394157bfd43713b18ff093d36cf4eb3b4385
Matthew Gilligan <mgilligan7043@student.pps.net>
2022-09-28 10:01:38 -0700

    worked on initial menu. started 2nd menu

---
+++ Assets/Scripts/Whatever.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whatever : MonoBehaviour
+{
+    public GameObject OptionsMenu = new GameObject("OptionsMenu");
+    // Start is called before the first frame update
+    void Start()
+    {
+        Update();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
