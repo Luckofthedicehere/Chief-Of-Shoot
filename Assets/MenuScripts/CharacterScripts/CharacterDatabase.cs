c4497bedb21b6af310279f12cc5650f612e6d4b6
Matthew Gilligan <mgilligan7043@student.pps.net>
2022-10-13 09:59:06 -0700

    Finished basic character select (working on adding all presidents)

--- Assets/MenuScripts/CharacterScripts/CharacterDatabase.cs
+++ Assets/MenuScripts/CharacterScripts/CharacterDatabase.cs
@@ -10,23 +10,59 @@ public class CharacterDatabase : MonoBehaviour
     [SerializeField] GameObject Canvas;
     [SerializeField] GameObject lockedCharacter;
 
-    
     public TextMesh nameText;
-    GameObject[] presidents;//look up"how to make an array of gameobjects unity, for the tutorial"
-    public int test = 2;
-    public int charactersTotal = 2;//update when adding a new character
-    public int SelectedCharacter = 0;
 
-    public int[] CharacterArray = new int[1]; //update when adding a new character
+    private int selectedOption = 0;
+    public GameObject[] presidents;
 
-    public int GetCharacter(int characterArray) //for determining the character 
+    private void Start()
     {
-        return CharacterArray[characterArray];
+     
+    }
+
+    //public int test = 2;
+   
+    public GameObject GetCharacter(int characterArray) //for determining the character 
+    {
+        return presidents[characterArray];
+    }
+
+    public void noCharacter(int president)
+    {
+        presidents[president].SetActive(false);
     }
 
     public void UpdateCharacter(int president)
     {
         presidents[president].SetActive(true);
     }
+
+     
+    public void nextCharacter()
+    {
+
+        noCharacter(selectedOption);
+        selectedOption++;
+        if (selectedOption > presidents.Length-1)
+        {
+            selectedOption = 0;
+        }
+        UpdateCharacter(selectedOption);
+
+    }
+
+    public void backChacarter()
+    {
+        noCharacter(selectedOption);
+        selectedOption--;
+
+
+        if (selectedOption < 0)
+        {
+            selectedOption = presidents.Length-1;
+        }
+       UpdateCharacter(selectedOption);
+    }
+
 
 }
--- Assets/MenuScripts/CharacterScripts/CharacterManager.cs
+++ Assets/MenuScripts/CharacterScripts/CharacterManager.cs
@@ -8,27 +8,5 @@ public class CharacterManager : MonoBehaviour
 
     public CharacterDatabase CharacterDB;
 
-    private int selectedOption = 0;
-
-    public void nextCharacter()
-    {
-        selectedOption++;
-        if (selectedOption > 2)
-        {
-            selectedOption = 0;
-        }
-        CharacterDB.UpdateCharacter(selectedOption);
-
-    }
-
-    public void backChacarter()
-    {
-        selectedOption--;
-        if (selectedOption < 0)
-        {
-            selectedOption = 2;   
-        }
-        CharacterDB.UpdateCharacter(selectedOption);
-    }
-
+    
 }
